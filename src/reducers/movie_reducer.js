7d5aedb6cbbea4e57b3f560ac7f281d8dc3ee55b
Justen Quirante <jcquirante@gmail.com>
2019-01-16 17:53:22 -0800

    Completed user-auth

--- src/reducers/movie_reducer.js
+++ src/reducers/movie_reducer.js
@@ -6,6 +6,8 @@ const DEFAULT_STATE = {
 
 export default (state = DEFAULT_STATE, action) => {
     switch (action.type) {
+        case types.GET_MOVIE_QUOTE:
+            return {...state, quote: action.quote}
         default:
             return state;
     }
